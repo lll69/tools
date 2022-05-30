4499adfcfc4033f04ec03be1d42314da48b80330
lll69 <60803753+lll69@users.noreply.github.com>
2022-05-30 13:33:46 +0800

    Update

---
+++ res/h.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         高度限制
+// @description  高度限制
+// @namespace    https://lll69.github.io/
+// @version      1.0
+// @author       lll69
+// @updateURL    https://lll69.github.io/tools/res/h.user.js
+// @match        https://gitblock.cn/*
+// @match        https://aerfaying.com/*
+// @match        https://3eworld.cn/*
+// @match        http://gitblock.cn/*
+// @match        http://aerfaying.com/*
+// @match        http://3eworld.cn/*
+// @icon         https://aerfaying.com/Content/logo.ico
+// @grant        none
+// @run-at       document-end
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    var s=document.createElement("style");
+    s.innerHTML=".user-home_userInfo_2szc4{max-height:max-content!important}"
+    document.head.appendChild(s);
+})();
